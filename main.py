dbb955592cb4eb32b9ad2d9d2fa077c7bcec63c2
mathijsschoool <75422017+mathijsschoool@users.noreply.github.com>
2020-12-08 15:57:02 +0100

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 def on_button_pressed_a():
-    laat_motor_draaien(1,1000, 1000)
+    laat_motor_draaien(1, 1000, pins.analog_read_pin(AnalogPin.P3))
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
-def laat_motor_draaien(port : number,time_milliseconds: number, speed: number):
-    portlist = [AnalogPin.P16,AnalogPin.P15,AnalogPin.P14,AnalogPin.P13]
-    pins.analog_write_pin(portlist[port], 0)
-    pins.analog_write_pin(portlist[port], speed)
+def laat_motor_draaien(port: number, time_milliseconds: number, speed: number):
+    global portlist
+    portlist = [AnalogPin.P16, AnalogPin.P15, AnalogPin.P14, AnalogPin.P13]
+    pins.analog_write_pin(portlist[port - 1], 0)
+    pins.analog_write_pin(portlist[port - 1], speed)
     basic.pause(time_milliseconds)
-    pins.analog_write_pin(portlist[port], 0)
-
+    pins.analog_write_pin(portlist[port - 1], 0)
 
 def on_forever():
     pass
